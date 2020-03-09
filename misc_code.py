1ed8021f033eecc66daca962151a28447f806c31
Atharva Kale <atharvakale1857@gmail.com>
2020-03-09 17:52:01 +0530

    Create misc_code.py

---
+++ misc_code.py
@@ -0,0 +1,11 @@
+plt.figure(figsize=(20, 10)) 
+plt.subplots_adjust(bottom=0.20) 
+# plt.bar(most_used_dev.keys(), most_used_dev.values(), color="xkcd:turquoise",) 
+for k in most_used_dev.keys(): 
+	plt.bar(k, most_used_dev[k], color=(np.random.random(), np.random.random(), np.random.random()), label=str(most_used_dev[k])) 
+plt.xticks(rotation=45)
+plt.title("Device - App Combination x Usage")
+plt.xlabel("Platforms")
+plt.ylabel("# of Sessions")
+plt.legend()
+plt.show()
